6a7b4b08c9cdd132a9cbbb6deacc6ecc3d0e8048
Jacob Hummer <jcbhmr@outlook.com>
2023-08-16 00:45:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 set -e
 
+apt_get_update() {
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+check_packages curl unzip
+
 echo "Activating feature 'deno'"
 
 TARGET_DENO_VERSION=${VERSION:-"latest"}
