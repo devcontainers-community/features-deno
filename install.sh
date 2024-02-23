ce3d7f680222505da9b385ad287ad8634ace7b69
toms74209200 <motomotomato@gmail.com>
2024-02-23 14:07:16 +0900

    Update install.sh to using officially ARM build

--- install.sh
+++ install.sh
@@ -21,10 +21,15 @@ check_packages curl unzip ca-certificates
 echo "Activating feature 'deno'"
 
 TARGET_DENO_VERSION=${VERSION:-"latest"}
+MIN_ARM_SUPPORT_VERSION="1.41.0"
+COMPARE_MIN_VERSION=$(echo -e "${TARGET_DENO_VERSION}\n${MIN_ARM_SUPPORT_VERSION}" | sort -V | head -n1)
 
-if [ "${TARGET_DENO_VERSION}" = "latest" ] || "${TARGET_DENO_VERSION}" = "none";
+if [ "${TARGET_DENO_VERSION}" = "latest" ] || [ "${TARGET_DENO_VERSION}" = "none" ];
 then
-    curl -fsSL https://gist.githubusercontent.com/LukeChannings/09d53f5c364391042186518c8598b85e/raw/ac8cd8c675b985edd4b3e16df63ffef14d1f0e24/deno_install.sh | DENO_INSTALL=/usr/local sh
+    curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=/usr/local sh
+elif [ "${TARGET_DENO_VERSION}" = "${MIN_ARM_SUPPORT_VERSION}" ] || [ "${COMPARE_MIN_VERSION}" = "${MIN_ARM_SUPPORT_VERSION}" ];
+then
+    curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=/usr/local sh -s "v${TARGET_DENO_VERSION}"
 else
     curl -fsSL https://gist.githubusercontent.com/LukeChannings/09d53f5c364391042186518c8598b85e/raw/ac8cd8c675b985edd4b3e16df63ffef14d1f0e24/deno_install.sh | DENO_INSTALL=/usr/local sh -s "v${TARGET_DENO_VERSION}"
 fi
